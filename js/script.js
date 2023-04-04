12e7c0437ed1c101c6a6345690aa22d1ef526b05
Abhishek kumar singh <abhisheksinghabhibbui@gmail.com>
2023-04-04 20:37:14 +0530

    update

--- js/script.js
+++ js/script.js
@@ -9,17 +9,23 @@ const toggleNavbar = () => {
 mobile_nav.addEventListener('click', () => toggleNavbar());
 
 function sendEmail(){
-    console.log("hello");
+    let name= document.getElementById("clientname").value;
+    let email= document.getElementById("clientemail").value;
+    let phone= document.getElementById("clientphone").value;
+    let details= document.getElementById("details").value;
+    let checkbox= document.getElementById("fromlko").value;
+
+
+
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "singhabhisheklko315@gmail.com",
-        Password : "969D6A1DBF3F35BF7967EBA6967B052B4F4D",
-        To : "singhabhisheklko315@gmail.com",
+        SecureToken :"deba718d-f6f1-4510-a782-9d99f15b727c",
         From : "singhabhisheklko315@gmail.com",
+        To : "singhabhisheklko315@gmail.com",
         Subject : "lucknow foods enquiry",
-        Body : "Name:" + document.getElementById("clientname").value + "<br> Email: " +document.getElementById("clientemail").value + "<br> phone no: " + document.getElementById("clientphone").value + "<br> Messages: " + document.getElementById("details").value + "<br> from lucknow:" + document.getElementById("fromlko").value
+        Body : "NAME:" + name + "<br> Email: " + email +"__"+"__"+ phone+ "__" + details + "__"+ checkbox
     }).then(
         message => alert("Query Submitted Succesfully")
     );
    
 }
+
